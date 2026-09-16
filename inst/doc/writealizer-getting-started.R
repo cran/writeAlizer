8005efc0a46ae6705fa99ff2b655159630fafb60
Sterett H. Mercer <sterett.mercer@ubc.ca>
2026-09-16 23:30:02 +0000

    version 1.7.4

--- inst/doc/writealizer-getting-started.R
+++ inst/doc/writealizer-getting-started.R
@@ -1,8 +1,27 @@
 ## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>",
-  # Run chunks locally/CI when NOT_CRAN=true; skip on CRAN
-  eval = identical(Sys.getenv("NOT_CRAN"), "true")
-)
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
+
+## ----load-package-------------------------------------------------------------
+library(writeAlizer)
+
+## ----import-sample------------------------------------------------------------
+rb_path <- system.file("extdata", "sample_rb.csv", package = "writeAlizer")
+rb <- import_rb(rb_path)
+head(rb$ID)
+
+## ----offline-demo-------------------------------------------------------------
+local({
+  old <- options(writeAlizer.mock_dir = NULL, writeAlizer.offline = TRUE)
+  on.exit(options(old))
+  example_parent <- tempfile("wa-example-")
+  wa_seed_example_models(dir = example_parent)
+  on.exit(unlink(example_parent, recursive = TRUE), add = TRUE)
+  demo <- predict_quality("example", rb)
+  head(demo)
+})
+
+## ----import-all---------------------------------------------------------------
+rb <- import_rb(system.file("extdata", "sample_rb.csv", package = "writeAlizer"))
+coh <- import_coh(system.file("extdata", "sample_coh.csv", package = "writeAlizer"))
+gam <- import_gamet(system.file("extdata", "sample_gamet.csv", package = "writeAlizer"))
 
